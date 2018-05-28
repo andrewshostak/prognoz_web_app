eb63c51c89d5859759ed16921c16d6148e1878cb
Andriy Shostak <andriy.shostak@vakoms.com.ua>
2018-05-28 19:56:46 +0300

    Cup rules

--- src/app/cup/cup-rules/cup-rules.component.ts
+++ src/app/cup/cup-rules/cup-rules.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit }    from '@angular/core';
 import { TitleService }         from '../../core/title.service';
 
+import { CupStageType }         from '../../shared/models/cup-stage-type.model';
+import { CupStageTypeService }  from '../../core/services/cup/cup-stage-type.service';
+
 @Component({
   selector: 'app-cup-rules',
   templateUrl: './cup-rules.component.html',
@@ -9,11 +12,23 @@ import { TitleService }         from '../../core/title.service';
 export class CupRulesComponent implements OnInit {
 
     constructor(
-        private titleService: TitleService
+        private cupStageTypeService: CupStageTypeService,
+        private titleService: TitleService,
     ) { }
+
+    cupStageTypes: CupStageType[];
+    errorCupStageTypes: string;
 
     ngOnInit() {
         this.titleService.setTitle('Правила конкурсу - Кубок');
+        this.cupStageTypeService.getCupStageTypes().subscribe(
+            response => {
+                this.cupStageTypes = response;
+            },
+            error => {
+                this.errorCupStageTypes = error;
+            }
+        );
     }
 
 }
