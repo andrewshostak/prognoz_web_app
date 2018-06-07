61e89cec712597d300cc1d34a9d40363595ee65d
Andriy Shostak <andriy.shostak@vakoms.com.ua>
2018-06-07 21:39:21 +0300

    Husky precommit test

--- src/app/cup/cup-applications/cup-applications.component.ts
+++ src/app/cup/cup-applications/cup-applications.component.ts
@@ -1,29 +1,28 @@
 import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
 
-import { AuthService }                              from '../../core/auth.service';
-import { Competition }                              from '../../shared/models/competition.model';
-import { CompetitionService }                       from '../../core/competition.service';
-import { ConfirmModalService }                      from '../../core/confirm-modal.service';
-import { CupApplicationService }                    from './cup-application.service';
-import { CupApplication }                           from '../../shared/models/cup-application.model';
-import { CurrentStateService }                      from '../../core/current-state.service';
-import { environment }                              from '../../../environments/environment';
-import { forkJoin }                                 from 'rxjs/observable/forkJoin';
-import { HelperService }                            from '../../core/helper.service';
-import { NotificationsService }                     from 'angular2-notifications';
-import { Subscription }                             from 'rxjs/Subscription';
-import { TitleService }                             from '../../core/title.service';
-import { User }                                     from '../../shared/models/user.model';
+import { AuthService } from '../../core/auth.service';
+import { Competition } from '../../shared/models/competition.model';
+import { CompetitionService } from '../../core/competition.service';
+import { ConfirmModalService } from '../../core/confirm-modal.service';
+import { CupApplicationService } from './cup-application.service';
+import { CupApplication } from '../../shared/models/cup-application.model';
+import { CurrentStateService } from '../../core/current-state.service';
+import { environment } from '../../../environments/environment';
+import { forkJoin } from 'rxjs/observable/forkJoin';
+import { HelperService } from '../../core/helper.service';
+import { NotificationsService } from 'angular2-notifications';
+import { Subscription } from 'rxjs/Subscription';
+import { TitleService } from '../../core/title.service';
+import { User } from '../../shared/models/user.model';
 
 declare const $: any;
 
 @Component({
-  selector: 'app-cup-applications',
-  templateUrl: './cup-applications.component.html',
-  styleUrls: ['./cup-applications.component.css']
+    selector: 'app-cup-applications',
+    templateUrl: './cup-applications.component.html',
+    styleUrls: ['./cup-applications.component.css']
 })
 export class CupApplicationsComponent implements OnInit, OnDestroy {
-
     constructor(
         private authService: AuthService,
         private competitionService: CompetitionService,
@@ -34,7 +33,7 @@ export class CupApplicationsComponent implements OnInit, OnDestroy {
         private helperService: HelperService,
         private notificationService: NotificationsService,
         private titleService: TitleService
-    ) { }
+    ) {}
 
     authenticatedUser: User;
     competitions: Competition[];
@@ -42,76 +41,71 @@ export class CupApplicationsComponent implements OnInit, OnDestroy {
     errorCupApplications: string;
     selectedCompetitionForNewApplication: Competition;
     selectedCupApplication: {
-        competition_id: number,
-        applicant_id: number,
-        receiver_id?: number,
-        place?: number,
-        id?: number
+        competition_id: number;
+        applicant_id: number;
+        receiver_id?: number;
+        place?: number;
+        id?: number;
     };
     userImageDefault: string;
-    userImagesUrl:  string;
+    userImagesUrl: string;
     userSubscription: Subscription;
 
     attachApplicationsToCompetitions(): void {
         if (this.competitions.length && !this.noCupApplications()) {
             const grouped = this.helperService.groupBy(this.cupApplications, cupApplication => cupApplication.competition_id);
-            this.competitions.map((competition) => {
-                return competition.cup_applications = grouped.get(competition.id);
+            this.competitions.map(competition => {
+                return (competition.cup_applications = grouped.get(competition.id));
             });
         }
     }
 
     confirmApplication(cupApplication: CupApplication): void {
-        if (true) { console.log('remove this'); }
+        if (true) console.log('remove this');
         //test comment
-        const updateRequestData = { competition_id: cupApplication.competition_id, receiver_id: this.authenticatedUser.id, confirmed: true };
-        this.confirmModalService.show(
-            () => {
-                this.cupApplicationService
-                    .updateCupApplication(updateRequestData, cupApplication.id)
-                    .subscribe(
-                        () => {
-                            this.getApplicationsAndCompetitions();
-                            this.confirmModalService.hide();
-                            this.notificationService.success('Успішно', 'Заявку підтверджено');
-                        },
-                        error => {
-                            this.getApplicationsAndCompetitions();
-                            this.confirmModalService.hide();
-                            this.notificationService.error('Помилка', error);
-                        });
-            },
-            `Підтвердити заявку ${cupApplication.applicant.name}?`
-        );
+        const updateRequestData = {
+            competition_id: cupApplication.competition_id,
+            receiver_id: this.authenticatedUser.id,
+            confirmed: true
+        };
+        this.confirmModalService.show(() => {
+            this.cupApplicationService.updateCupApplication(updateRequestData, cupApplication.id).subscribe(
+                () => {
+                    this.getApplicationsAndCompetitions();
+                    this.confirmModalService.hide();
+                    this.notificationService.success('Успішно', 'Заявку підтверджено');
+                },
+                error => {
+                    this.getApplicationsAndCompetitions();
+                    this.confirmModalService.hide();
+                    this.notificationService.error('Помилка', error);
+                }
+            );
+        }, `Підтвердити заявку ${cupApplication.applicant.name}?`);
     }
 
     deleteCupApplication(competition: Competition, cupApplication: CupApplication): void {
         if (this.showEditAndDeleteButtons(competition, cupApplication)) {
             this.confirmModalService.show(() => {
-                    this.cupApplicationService
-                        .deleteCupApplication(cupApplication.id)
-                        .subscribe(
-                            response => {
-                                this.getApplicationsAndCompetitions();
-                                this.confirmModalService.hide();
-                                this.notificationService.success('Успішно', 'Заявку видалено');
-                            },
-                            error => {
-                                this.getApplicationsAndCompetitions();
-                                this.confirmModalService.hide();
-                                this.notificationService.error('Помилка', error);
-                            }
-                    );
-                },
-                `Видалити заявку ${cupApplication.applicant.name}?`
-            );
+                this.cupApplicationService.deleteCupApplication(cupApplication.id).subscribe(
+                    response => {
+                        this.getApplicationsAndCompetitions();
+                        this.confirmModalService.hide();
+                        this.notificationService.success('Успішно', 'Заявку видалено');
+                    },
+                    error => {
+                        this.getApplicationsAndCompetitions();
+                        this.confirmModalService.hide();
+                        this.notificationService.error('Помилка', error);
+                    }
+                );
+            }, `Видалити заявку ${cupApplication.applicant.name}?`);
         }
     }
 
     getApplicationsAndCompetitions(): void {
         const cupApplications = this.cupApplicationService.getCupApplications();
-        const competitions = this.competitionService
-            .getCompetitions(null, environment.tournaments.cup.id, null, null, true, true);
+        const competitions = this.competitionService.getCompetitions(null, environment.tournaments.cup.id, null, null, true, true);
 
         forkJoin([cupApplications, competitions]).subscribe(
             response => {
@@ -145,12 +139,14 @@ export class CupApplicationsComponent implements OnInit, OnDestroy {
             return false;
         }
 
-        return this.authenticatedUser &&
+        return (
+            this.authenticatedUser &&
             !this.hasRefusedApplication(competition, this.authenticatedUser) &&
             !this.hasUnConfirmedRequisition(this.authenticatedUser) &&
             !this.hasConfirmedRequisitionAsApplicant(this.authenticatedUser) &&
             !this.hasConfirmedRequisitionAsReceiver(this.authenticatedUser) &&
-            competition.stated;
+            competition.stated
+        );
     }
 
     hasModeratorRights(): boolean {
@@ -161,37 +157,35 @@ export class CupApplicationsComponent implements OnInit, OnDestroy {
         if (this.noCupApplications()) {
             return false;
         }
-        return this.cupApplications
-            .some((cupApplication) => cupApplication.receiver_id === receiver.id && !!cupApplication.confirmed_at);
+        return this.cupApplications.some(cupApplication => cupApplication.receiver_id === receiver.id && !!cupApplication.confirmed_at);
     }
 
     hasConfirmedRequisitionAsApplicant(applicant: User): boolean {
         if (this.noCupApplications()) {
             return false;
         }
-        return this.cupApplications
-            .some((cupApplication) => cupApplication.applicant_id === applicant.id && !!cupApplication.confirmed_at);
+        return this.cupApplications.some(cupApplication => cupApplication.applicant_id === applicant.id && !!cupApplication.confirmed_at);
     }
 
     hasRefusedApplication(competition: Competition, applicant: User): boolean {
         if (this.noCupApplications()) {
             return false;
         }
-        return !this.isFriendlyCompetition(competition) && this.cupApplications.some((cupApplication) => {
+        return (
+            !this.isFriendlyCompetition(competition) &&
+            this.cupApplications.some(cupApplication => {
                 return cupApplication.applicant_id === applicant.id && !!cupApplication.refused_at;
-            });
+            })
+        );
     }
 
     hasUnConfirmedRequisition(applicant: User): boolean {
         if (this.noCupApplications()) {
             return false;
         }
-        return this.cupApplications
-            .some((cupApplication) => {
-                return cupApplication.applicant_id === applicant.id &&
-                    !cupApplication.confirmed_at &&
-                    !cupApplication.refused_at;
-            });
+        return this.cupApplications.some(cupApplication => {
+            return cupApplication.applicant_id === applicant.id && !cupApplication.confirmed_at && !cupApplication.refused_at;
+        });
     }
 
     ngOnDestroy() {
@@ -224,9 +218,9 @@ export class CupApplicationsComponent implements OnInit, OnDestroy {
             this.selectedCupApplication = {
                 competition_id: competition.id,
                 applicant_id: this.authenticatedUser.id,
-                place: this.isFriendlyCompetition(competition) ?
-                    environment.tournaments.cup.places.find(place => place.slug === 'anywhere').id :
-                    null
+                place: this.isFriendlyCompetition(competition)
+                    ? environment.tournaments.cup.places.find(place => place.slug === 'anywhere').id
+                    : null
             };
         } else {
             this.selectedCupApplication = {
@@ -245,24 +239,20 @@ export class CupApplicationsComponent implements OnInit, OnDestroy {
             competition_id: cupApplication.competition_id,
             refused: true
         };
-        this.confirmModalService.show(
-            () => {
-                this.cupApplicationService
-                    .updateCupApplication(updateRequestData, cupApplication.id)
-                    .subscribe(
-                        () => {
-                            this.getApplicationsAndCompetitions();
-                            this.confirmModalService.hide();
-                            this.notificationService.success('Успішно', 'Заявку відхилено');
-                        },
-                        error => {
-                            this.getApplicationsAndCompetitions();
-                            this.confirmModalService.hide();
-                            this.notificationService.error('Помилка', error);
-                        });
-            },
-            `Відхилити заявку ${cupApplication.applicant.name}?`
-        );
+        this.confirmModalService.show(() => {
+            this.cupApplicationService.updateCupApplication(updateRequestData, cupApplication.id).subscribe(
+                () => {
+                    this.getApplicationsAndCompetitions();
+                    this.confirmModalService.hide();
+                    this.notificationService.success('Успішно', 'Заявку відхилено');
+                },
+                error => {
+                    this.getApplicationsAndCompetitions();
+                    this.confirmModalService.hide();
+                    this.notificationService.error('Помилка', error);
+                }
+            );
+        }, `Відхилити заявку ${cupApplication.applicant.name}?`);
     }
 
     showActionButtons(competition: Competition, cupApplication: CupApplication): boolean {
@@ -319,9 +309,7 @@ export class CupApplicationsComponent implements OnInit, OnDestroy {
         return false;
     }
 
-
     private noCupApplications(): boolean {
         return !this.cupApplications || !this.cupApplications.length;
     }
-
 }
