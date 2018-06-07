88096db7f267cb3a79dd3bd7b7cd00403683edeb
Andriy Shostak <andriy.shostak@vakoms.com.ua>
2018-06-07 21:45:24 +0300

    test 2

--- src/app/cup/cup-applications/cup-applications.component.ts
+++ src/app/cup/cup-applications/cup-applications.component.ts
@@ -61,10 +61,7 @@ export class CupApplicationsComponent implements OnInit, OnDestroy {
     }
 
     confirmApplication(cupApplication: CupApplication): void {
-        if (true)
-            console.log(
-                'remove this kmas dlda lllllllllllllllllllllmldsma kla madsads das ak slm kasdkmld aslads kmasdlkmasdlkmsadlkmadslkmlkmdaslkmdsa'
-            );
+        if (true) console.log('remove this kmas dlda lllllllllllllllllllllmldsma kla madsads das ak slm kasdkmld aslads kmasdlkmasdlkmsadlkmadslkmlkmdaslkmdsa');
         //test comment  asddas  adsasddddddddddddddddddddddddddddddd
         const updateRequestData = {
             competition_id: cupApplication.competition_id,
