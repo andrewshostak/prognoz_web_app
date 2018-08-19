4d7c0548349841ea3b19c16167e0dac6b5dfe79c
Andriy Shostak <andrew.shostak11@gmail.com>
2018-08-19 14:24:30 +0300

    cup rating group: navigation

--- src/app/cup/cup-rating-group/cup-rating-group.component.ts
+++ src/app/cup/cup-rating-group/cup-rating-group.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { CupRatingGroup } from '@models/cup/cup-rating-group.model';
 import { CupRatingService } from '@services/cup/cup-rating.service';
@@ -14,16 +14,37 @@ import { UtilsService } from '@services/utils.service';
     styleUrls: ['./cup-rating-group.component.scss']
 })
 export class CupRatingGroupComponent implements OnInit {
-    constructor(private activatedRoute: ActivatedRoute, private cupRatingService: CupRatingService, private titleService: TitleService) {}
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private cupRatingService: CupRatingService,
+        private router: Router,
+        private titleService: TitleService
+    ) {}
 
     activatedRouteSubscription: Subscription;
     competitionId: number;
     cupRatingGroup: CupRatingGroup[];
     errorRatingGroup: string;
+    errorGroupNumbers: string;
     getHomeCityInBrackets = UtilsService.getHomeCityInBrackets;
     groupNumber: number;
+    groupNumbers: number[];
     userImageDefault: string;
     userImagesUrl: string;
+
+    groupChanged(groupNumber: string): void {
+        this.router.navigate(['/cup', this.competitionId, 'rating-group', groupNumber]);
+    }
+
+    nextGroup(selectedGroupNumber: string): void {
+        const groupNumber = parseInt(selectedGroupNumber, 10);
+        const index = this.groupNumbers.findIndex(item => item === groupNumber);
+        if (index === this.groupNumbers.length - 1 || !this.groupNumbers[index + 1]) {
+            return;
+        }
+
+        this.router.navigate(['/cup', this.competitionId, 'rating-group', this.groupNumbers[index + 1]]);
+    }
 
     ngOnInit() {
         this.userImageDefault = environment.imageUserDefault;
@@ -31,6 +52,9 @@ export class CupRatingGroupComponent implements OnInit {
         this.activatedRouteSubscription = this.activatedRoute.params.subscribe((params: Params) => {
             this.competitionId = params['competitionId'];
             this.groupNumber = params['groupNumber'];
+            if (!this.groupNumbers) {
+                this.getCupRatingGroups(this.competitionId);
+            }
             this.titleService.setTitle(`Рейтинг групи ${this.groupNumber} - Кубок`);
             this.cupRatingService.getCupRatingGroup(this.competitionId, this.groupNumber).subscribe(
                 response => {
@@ -41,5 +65,26 @@ export class CupRatingGroupComponent implements OnInit {
                 }
             );
         });
+    }
+
+    previousGroup(selectedGroupNumber: string): void {
+        const groupNumber = parseInt(selectedGroupNumber, 10);
+        const index = this.groupNumbers.findIndex(item => item === groupNumber);
+        if (index === 0 || !this.groupNumbers[index - 1]) {
+            return;
+        }
+
+        this.router.navigate(['/cup', this.competitionId, 'rating-group', this.groupNumbers[index - 1]]);
+    }
+
+    private getCupRatingGroups(competitionId: number) {
+        this.cupRatingService.getCupRatingGroups(competitionId).subscribe(
+            response => {
+                this.groupNumbers = response;
+            },
+            error => {
+                this.errorGroupNumbers = error;
+            }
+        );
     }
 }
