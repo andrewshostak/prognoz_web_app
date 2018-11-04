b3260d60211633e3647303c3d973a24996304b66
Andriy Shostak <andrew.shostak11@gmail.com>
2018-11-04 14:15:00 +0200

    login form removed from header.

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { AuthService } from '@services/auth.service';
 import { NotificationsService } from 'angular2-notifications';
@@ -13,13 +12,11 @@ import { User } from '@models/user.model';
 export class HeaderComponent implements OnInit {
     constructor(private authService: AuthService, private notificationsService: NotificationsService) {}
 
-    headerSignInForm: FormGroup;
-    spinnerButton = false;
     user: User;
 
     logout() {
         this.authService.logout().subscribe(
-            response => {
+            () => {
                 this.notificationsService.info('Успішно', 'Ви вийшли зі свого аккаунту');
             },
             error => {
@@ -30,27 +27,5 @@ export class HeaderComponent implements OnInit {
 
     ngOnInit() {
         this.authService.getUser.subscribe(response => (this.user = response));
-        this.headerSignInForm = new FormGroup({
-            name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-            password: new FormControl('', [Validators.required])
-        });
-    }
-
-    onSubmit() {
-        if (this.headerSignInForm.valid) {
-            this.spinnerButton = true;
-            this.authService.signIn(this.headerSignInForm.value.name, this.headerSignInForm.value.password).subscribe(
-                response => {
-                    this.notificationsService.success('Успішно', 'Вхід виконано успішно');
-                    this.spinnerButton = false;
-                },
-                errors => {
-                    for (const error of errors) {
-                        this.notificationsService.error('Помилка', error);
-                    }
-                    this.spinnerButton = false;
-                }
-            );
-        }
     }
 }
