820d758a38bee1df510fa8d2ed2d18bc86c46628
Andriy Shostak <andrew.shostak11@gmail.com>
2019-05-11 12:00:21 +0300

    bash command changed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-echo "deployed"
-
-ls ~/prognoz_v2_angular -al
-
 # Exit if any errors are hit (any non-zero responses)
-#set -e
+set -e
 
 # Copy files via scp
-#scp -r ~/prognoz_v2_angular/dist root@46.101.159.170:/var/www/tmp-ui
+scp -r ~/prognoz_v2_angular/dist root@46.101.159.170:/var/www/tmp-ui
 
 # Change rights and move to files
-#ssh root@46.101.159.170 'sudo chown -R root:www-data /var/www/tmp-ui/ && rm -rf /var/www/ui/dist && mv /var/www/tmp-ui/dist/ /var/www/ui/dist/'
+ssh root@46.101.159.170 'sudo chown -R root:www-data /var/www/tmp-ui/ && rm -rf /var/www/ui/dist && mv /var/www/tmp-ui/dist/ /var/www/ui/dist/'
