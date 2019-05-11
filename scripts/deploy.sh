d5c11a545799b11427f2f9a797aad2b87444cde7
Andriy Shostak <andrew.shostak11@gmail.com>
2019-05-11 14:57:46 +0300

    ssh-keyscan

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,9 @@
 # Exit if any errors are hit (any non-zero responses)
 set -e
 
+# This is for ssh connection
+ssh-keyscan -H 46.101.159.170 >> ~/.ssh/known_hosts
+
 # Copy files via scp
 scp -r ~/prognoz_v2_angular/dist root@46.101.159.170:/var/www/tmp-ui
 
