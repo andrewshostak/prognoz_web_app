44c91f2053ee337f02eec7dc732d87f07635e900
Andriy Shostak <andrew.shostak11@gmail.com>
2019-05-11 15:21:36 +0300

    env vars

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # This is for ssh connection
-ssh-keyscan -H 46.101.159.170 >> ~/.ssh/known_hosts
+ssh-keyscan -H ${SITE_SSH_HOST} >> ~/.ssh/known_hosts
 
 # Copy files via scp
-scp -r ~/prognoz_v2_angular/dist root@46.101.159.170:/var/www/tmp-ui
+scp -r ~/prognoz_v2_angular/dist ${SITE_SSH_USER}@${SITE_SSH_HOST}:/var/www/tmp-ui
 
 # Change rights and move to files
-ssh root@46.101.159.170 'sudo chown -R root:www-data /var/www/tmp-ui/ && rm -rf /var/www/ui/dist && mv /var/www/tmp-ui/dist/ /var/www/ui/dist/'
+ssh ${SITE_SSH_USER}@${SITE_SSH_HOST} 'sudo chown -R root:www-data /var/www/tmp-ui/ && rm -rf /var/www/ui/dist && mv /var/www/tmp-ui/dist/ /var/www/ui/dist/'
