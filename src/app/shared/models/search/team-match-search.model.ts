6247760537b0ceab239b477c0e6c88b59e37d4b6
Andriy Shostak <andrew.shostak11@gmail.com>
2019-08-10 15:48:54 +0300

    team-matches form: don't show already added matches in the dropdown

--- src/app/shared/models/search/team-match-search.model.ts
+++ src/app/shared/models/search/team-match-search.model.ts
@@ -1,6 +1,8 @@
+import { ModelStatus } from '@enums/model-status.enum';
 import { Sequence } from '@enums/sequence.enum';
 
 export class TeamMatchSearch {
+   public active?: ModelStatus;
    public competitionId?: number;
    public limit: number;
    public orderBy?: string;
