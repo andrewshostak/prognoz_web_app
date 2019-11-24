18438f2a073e6bda3504460f6fe82586922fabe6
Andriy Shostak <andrew.shostak11@gmail.com>
2019-11-24 18:48:12 +0200

    team-create: empty page

--- src/app/team/team.module.ts
+++ src/app/team/team.module.ts
@@ -2,50 +2,52 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { SharedModule } from '../shared/shared.module';
+import { SharedModule } from '@app/shared/shared.module';
+import { TeamEditModalComponent } from '@team/shared/team-edit-modal/team-edit-modal.component';
+import { TeamGoalkeeperFormComponent } from '@team/shared/team-goalkeeper-form/team-goalkeeper-form.component';
+import { TeamPredictionFormComponent } from '@team/shared/team-prediction-form/team-prediction-form.component';
+import { TeamRatingTableComponent } from '@team/shared/team-rating-table/team-rating-table.component';
+import { TeamRatingUserTableComponent } from '@team/shared/team-rating-user-table/team-rating-user-table.component';
+import { TeamResultsTableComponent } from '@team/shared/team-results-table/team-results-table.component';
+import { TeamSelectModalComponent } from '@team/shared/team-select-modal/team-select-modal.component';
+import { TeamTeamMatchCardComponent } from '@team/shared/team-team-match-card/team-team-match-card.component';
+import { TeamCaptainComponent } from '@team/team-captain/team-captain.component';
+import { TeamCompetitionSelectComponent } from '@team/team-competition-select/team-competition-select.component';
+import { TeamCreateComponent } from '@team/team-create/team-create.component';
+import { TeamMatchesComponent } from '@team/team-matches/team-matches.component';
+import { TeamMyComponent } from '@team/team-my/team-my.component';
+import { TeamPredictionsComponent } from '@team/team-predictions/team-predictions.component';
+import { TeamRatingComponent } from '@team/team-rating/team-rating.component';
+import { TeamResultsComponent } from '@team/team-results/team-results.component';
+import { TeamRoutingModule } from '@team/team-routing.module';
+import { TeamRulesComponent } from '@team/team-rules/team-rules.component';
+import { TeamSquadsComponent } from '@team/team-squads/team-squads.component';
+import { TeamComponent } from '@team/team.component';
 import { SimpleNotificationsModule } from 'angular2-notifications';
-import { TeamCaptainComponent } from './team-captain/team-captain.component';
-import { TeamCompetitionSelectComponent } from './team-competition-select/team-competition-select.component';
-import { TeamComponent } from './team.component';
-import { TeamEditModalComponent } from './shared/team-edit-modal/team-edit-modal.component';
-import { TeamGoalkeeperFormComponent } from './shared/team-goalkeeper-form/team-goalkeeper-form.component';
-import { TeamMatchesComponent } from './team-matches/team-matches.component';
-import { TeamMyComponent } from './team-my/team-my.component';
-import { TeamPredictionFormComponent } from './shared/team-prediction-form/team-prediction-form.component';
-import { TeamPredictionsComponent } from './team-predictions/team-predictions.component';
-import { TeamRatingComponent } from './team-rating/team-rating.component';
-import { TeamRatingTableComponent } from './shared/team-rating-table/team-rating-table.component';
-import { TeamRatingUserTableComponent } from './shared/team-rating-user-table/team-rating-user-table.component';
-import { TeamResultsComponent } from './team-results/team-results.component';
-import { TeamResultsTableComponent } from './shared/team-results-table/team-results-table.component';
-import { TeamRoutingModule } from './team-routing.module';
-import { TeamRulesComponent } from './team-rules/team-rules.component';
-import { TeamSelectModalComponent } from './shared/team-select-modal/team-select-modal.component';
-import { TeamSquadsComponent } from './team-squads/team-squads.component';
-import { TeamTeamMatchCardComponent } from './shared/team-team-match-card/team-team-match-card.component';
 
 @NgModule({
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, SharedModule, SimpleNotificationsModule, TeamRoutingModule],
-    declarations: [
-        TeamCaptainComponent,
-        TeamCompetitionSelectComponent,
-        TeamComponent,
-        TeamEditModalComponent,
-        TeamGoalkeeperFormComponent,
-        TeamMatchesComponent,
-        TeamMyComponent,
-        TeamPredictionFormComponent,
-        TeamPredictionsComponent,
-        TeamRatingComponent,
-        TeamRatingTableComponent,
-        TeamRatingUserTableComponent,
-        TeamResultsComponent,
-        TeamResultsTableComponent,
-        TeamRulesComponent,
-        TeamSelectModalComponent,
-        TeamSquadsComponent,
-        TeamTeamMatchCardComponent
-    ],
-    exports: [TeamComponent]
+   imports: [CommonModule, FormsModule, ReactiveFormsModule, SharedModule, SimpleNotificationsModule, TeamRoutingModule],
+   declarations: [
+      TeamCaptainComponent,
+      TeamCompetitionSelectComponent,
+      TeamComponent,
+      TeamCreateComponent,
+      TeamEditModalComponent,
+      TeamGoalkeeperFormComponent,
+      TeamMatchesComponent,
+      TeamMyComponent,
+      TeamPredictionFormComponent,
+      TeamPredictionsComponent,
+      TeamRatingComponent,
+      TeamRatingTableComponent,
+      TeamRatingUserTableComponent,
+      TeamResultsComponent,
+      TeamResultsTableComponent,
+      TeamRulesComponent,
+      TeamSelectModalComponent,
+      TeamSquadsComponent,
+      TeamTeamMatchCardComponent
+   ],
+   exports: [TeamComponent]
 })
 export class TeamModule {}
