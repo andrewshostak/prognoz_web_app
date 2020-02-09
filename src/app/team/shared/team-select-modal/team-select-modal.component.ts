1a03cbb2ce833060540af52b0f1d2639ad4e72a5
Andriy Shostak <andrew.shostak11@gmail.com>
2020-02-09 17:40:47 +0200

    old team-select modal removed

--- src/app/team/shared/team-select-modal/team-select-modal.component.ts
+++ src/app/team/shared/team-select-modal/team-select-modal.component.ts
@@ -1,48 +1,70 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
-import { Team } from '@models/team/team.model';
-import { TeamService } from '@services/team/team.service';
-import { User } from '@models/user.model';
+import { TeamNew } from '@models/new/team-new.model';
+import { TeamSearch } from '@models/search/team-search.model';
+import { AuthNewService } from '@services/new/auth-new.service';
+import { TeamNewService } from '@services/new/team-new.service';
+import { SettingsService } from '@services/settings.service';
+import { UtilsService } from '@services/utils.service';
 
 @Component({
-    selector: 'app-team-select-modal',
-    templateUrl: './team-select-modal.component.html',
-    styleUrls: ['./team-select-modal.component.scss']
+   selector: 'app-team-select-modal',
+   templateUrl: './team-select-modal.component.html',
+   styleUrls: ['./team-select-modal.component.scss']
 })
 export class TeamSelectModalComponent implements OnInit {
-    @Input() authenticatedUser: User;
-    @Input() spinnerButton: boolean;
-    @Input() close: () => void;
-    @Output() submitted = new EventEmitter<FormGroup>();
+   @Input() public close: () => void;
+   @Output() public submitted = new EventEmitter<TeamNew>();
 
-    errorTeams: string;
-    noTeams = 'Ви не є капітаном жодної з команд.';
-    teamSelectForm: FormGroup;
-    teams: Team[];
+   public teams: TeamNew[];
+   public teamForm: FormGroup;
+   public showNotFoundMessage: boolean;
 
-    constructor(private teamService: TeamService) {}
+   constructor(private authService: AuthNewService, private teamService: TeamNewService) {}
 
-    ngOnInit() {
-        this.teamSelectForm = new FormGroup({
-            team_id: new FormControl(null, [Validators.required])
-        });
-        const param = [{ parameter: 'captain_id', value: this.authenticatedUser.id.toString() }];
-        this.teamService.getTeams(param).subscribe(
-            response => {
-                if (response) {
-                    this.teams = response.teams;
-                }
-            },
-            error => {
-                this.errorTeams = error;
-            }
-        );
-    }
+   public ngOnInit(): void {
+      this.teamForm = new FormGroup({ id: new FormControl(null, [Validators.required]) });
+      this.getTeamsData();
+   }
 
-    onSubmit() {
-        if (this.teamSelectForm.valid) {
-            this.submitted.emit(this.teamSelectForm);
-        }
-    }
+   public showFormErrorMessage(abstractControl: AbstractControl, errorKey: string): boolean {
+      return UtilsService.showFormErrorMessage(abstractControl, errorKey);
+   }
+
+   public showFormInvalidClass(abstractControl: AbstractControl): boolean {
+      return UtilsService.showFormInvalidClass(abstractControl);
+   }
+
+   public submit(): void {
+      if (this.teamForm.invalid) {
+         return;
+      }
+
+      const team = this.teams.find(t => t.id === parseInt(this.teamForm.get('id').value, 10));
+      this.submitted.emit(team);
+   }
+
+   private getTeamsData(): void {
+      const search: TeamSearch = {
+         captainId: this.authService.getUser().id,
+         page: 1,
+         limit: SettingsService.maxLimitValues.teamTeams
+      };
+      this.teamService.getTeams(search).subscribe(response => {
+         this.teams = response.data;
+         this.setTeamIdValue(this.teams);
+         this.setShowNotFoundMessage(this.teams);
+      });
+   }
+
+   private setShowNotFoundMessage(teams: TeamNew[]): void {
+      this.showNotFoundMessage = teams.length === 0;
+   }
+
+   private setTeamIdValue(teams: TeamNew[]): void {
+      if (teams.length === 1 && !(teams[0].stated || teams[0].confirmed)) {
+         this.teamForm.get('id').setValue(teams[0].id);
+      }
+   }
 }
