c47fbba99c8792a98226d99f24b5665c3f47c44a
Andriy Shostak <andrew.shostak11@gmail.com>
2020-07-12 22:18:03 +0300

    Remove unused 'updateChampionshipRatingItems' method from championship rating service

--- src/app/core/services/championship/championship-rating.service.ts
+++ src/app/core/services/championship/championship-rating.service.ts
@@ -1,61 +1,43 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
-import { catchError, map } from 'rxjs/operators';
-import { ChampionshipRating } from '@models/championship/championship-rating.model';
-import { ErrorHandlerService } from '@services/error-handler.service';
 import { environment } from '@env';
+import { ChampionshipRating } from '@models/championship/championship-rating.model';
+import { RequestParams } from '@models/request-params.model';
+import { ErrorHandlerService } from '@services/error-handler.service';
 import { HeadersWithToken } from '@services/headers-with-token.service';
 import { Observable } from 'rxjs';
-import { RequestParams } from '@models/request-params.model';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable()
 export class ChampionshipRatingService {
-    constructor(
-        private errorHandlerService: ErrorHandlerService,
-        private headersWithToken: HeadersWithToken,
-        private httpClient: HttpClient
-    ) {}
+   private championshipRatingUrl = environment.apiUrl + 'championship/rating';
 
-    private championshipRatingUrl = environment.apiUrl + 'championship/rating';
+   constructor(
+      private errorHandlerService: ErrorHandlerService,
+      private headersWithToken: HeadersWithToken,
+      private httpClient: HttpClient
+   ) {}
 
-    /**
-     * Get championship rating
-     * @param requestParams
-     * @returns {Observable<any>}
-     */
-    getChampionshipRatingItems(requestParams?: RequestParams[]): Observable<any> {
-        let params: HttpParams = new HttpParams();
-        if (requestParams) {
-            for (const requestParam of requestParams) {
-                params = params.append(requestParam.parameter, requestParam.value);
-            }
-        }
-        return this.httpClient.get(this.championshipRatingUrl, { params: params }).pipe(catchError(this.errorHandlerService.handle));
-    }
+   public getChampionshipRatingItems(requestParams?: RequestParams[]): Observable<any> {
+      let params: HttpParams = new HttpParams();
+      if (requestParams) {
+         for (const requestParam of requestParams) {
+            params = params.append(requestParam.parameter, requestParam.value);
+         }
+      }
+      return this.httpClient.get(this.championshipRatingUrl, { params }).pipe(catchError(this.errorHandlerService.handle));
+   }
 
-    /**
-     * Get championship rating item
-     * @param userId
-     * @param competitionId
-     * @returns {Observable<ChampionshipRating>}
-     */
-    getChampionshipRatingItem(userId: number, competitionId?: number): Observable<ChampionshipRating> {
-        let params: HttpParams = new HttpParams();
-        if (competitionId) {
-            params = params.append('competition_id', competitionId.toString());
-        }
-        return this.httpClient.get(`${this.championshipRatingUrl}/${userId}`, { params: params }).pipe(
-            map(response => response['championship_rating']),
-            catchError(this.errorHandlerService.handle)
-        );
-    }
-
-    /**
-     * Update positions and moving
-     * @returns {Observable<void>}
-     */
-    updateChampionshipRatingItems(): Observable<void> {
-        return this.headersWithToken.put(this.championshipRatingUrl, {}).pipe(catchError(this.errorHandlerService.handle));
-    }
+   public getChampionshipRatingItem(userId: number, competitionId?: number): Observable<ChampionshipRating> {
+      let params: HttpParams = new HttpParams();
+      if (competitionId) {
+         params = params.append('competition_id', competitionId.toString());
+      }
+      return this.httpClient.get(`${this.championshipRatingUrl}/${userId}`, { params }).pipe(
+         // tslint:disable-next-line:no-string-literal
+         map(response => response['championship_rating']),
+         catchError(this.errorHandlerService.handle)
+      );
+   }
 }
