7428d6314c016027dc0de6ae539b4bd03f93fb0e
Andriy Shostak <andrew.shostak11@gmail.com>
2020-09-27 13:41:48 +0300

    Mark old service methods as deprecated

--- src/app/core/services/cup/cup-cup-match.service.ts
+++ src/app/core/services/cup/cup-cup-match.service.ts
@@ -1,119 +1,88 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
-import { catchError, map } from 'rxjs/operators';
-import { CupCupMatch } from '@models/cup/cup-cup-match.model';
 import { environment } from '@env';
+import { CupCupMatch } from '@models/cup/cup-cup-match.model';
 import { ErrorHandlerService } from '@services/error-handler.service';
 import { HeadersWithToken } from '@services/headers-with-token.service';
-import { isNullOrUndefined } from 'util';
 import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { isNullOrUndefined } from 'util';
 
 @Injectable()
 export class CupCupMatchService {
-    constructor(
-        private errorHandlerService: ErrorHandlerService,
-        private headersWithToken: HeadersWithToken,
-        private httpClient: HttpClient
-    ) {}
+   private cupCupMatchUrl = `${environment.apiUrl}cup/cup-matches`;
+   constructor(
+      private errorHandlerService: ErrorHandlerService,
+      private headersWithToken: HeadersWithToken,
+      private httpClient: HttpClient
+   ) {}
 
-    private cupCupMatchUrl = `${environment.apiUrl}cup/cup-matches`;
+   /**
+    * @deprecated
+    */
+   public getCupCupMatches(
+      active?: boolean,
+      cupStageId?: number,
+      page?: number,
+      ended?: boolean,
+      order?: string,
+      sequence?: 'asc' | 'desc'
+   ): Observable<any> {
+      let params: HttpParams = new HttpParams();
+      if (page) {
+         params = params.append('page', page.toString());
+      }
+      if (!isNullOrUndefined(active)) {
+         params = params.append('active', active ? '1' : '0');
+      }
+      if (!isNullOrUndefined(ended)) {
+         params = params.append('ended', ended ? '1' : '0');
+      }
+      if (!isNullOrUndefined(cupStageId)) {
+         params = params.append('cup_stage_id', cupStageId.toString());
+      }
+      if (!isNullOrUndefined(order)) {
+         params = params.append('order', order);
+      }
+      if (!isNullOrUndefined(sequence)) {
+         params = params.append('sequence', sequence);
+      }
+      return this.httpClient.get(this.cupCupMatchUrl, { params }).pipe(catchError(this.errorHandlerService.handle));
+   }
 
-    /**
-     * Get cup cup matches
-     * @param active
-     * @param cupStageId
-     * @param page
-     * @param ended
-     * @param order
-     * @param sequence
-     * @returns {any}
-     */
-    getCupCupMatches(
-        active?: boolean,
-        cupStageId?: number,
-        page?: number,
-        ended?: boolean,
-        order?: string,
-        sequence?: 'asc' | 'desc'
-    ): Observable<any> {
-        let params: HttpParams = new HttpParams();
-        if (page) {
-            params = params.append('page', page.toString());
-        }
-        if (!isNullOrUndefined(active)) {
-            params = params.append('active', active ? '1' : '0');
-        }
-        if (!isNullOrUndefined(ended)) {
-            params = params.append('ended', ended ? '1' : '0');
-        }
-        if (!isNullOrUndefined(cupStageId)) {
-            params = params.append('cup_stage_id', cupStageId.toString());
-        }
-        if (!isNullOrUndefined(order)) {
-            params = params.append('order', order);
-        }
-        if (!isNullOrUndefined(sequence)) {
-            params = params.append('sequence', sequence);
-        }
-        return this.httpClient.get(this.cupCupMatchUrl, { params: params }).pipe(catchError(this.errorHandlerService.handle));
-    }
+   /**
+    * @deprecated
+    */
+   public getCupCupMatch(cupCupMatchId: number): Observable<CupCupMatch> {
+      return this.httpClient.get(`${this.cupCupMatchUrl}/${cupCupMatchId}`).pipe(
+         map(response => response.cup_cup_match),
+         catchError(this.errorHandlerService.handle)
+      );
+   }
 
-    /**
-     * Get cup cup match
-     * @param {number} cupCupMatchId
-     * @returns {Observable<CupCupMatch>}
-     */
-    getCupCupMatch(cupCupMatchId: number): Observable<CupCupMatch> {
-        return this.httpClient.get(`${this.cupCupMatchUrl}/${cupCupMatchId}`).pipe(
-            map(response => response['cup_cup_match']),
-            catchError(this.errorHandlerService.handle)
-        );
-    }
+   public createCupCupMatchesAuto(params: { type: 'auto' | 'manual'; to: number; number_of_matches: number }): Observable<CupCupMatch[]> {
+      return this.headersWithToken.post(this.cupCupMatchUrl, params).pipe(
+         map(response => response.cup_cup_matches),
+         catchError(this.errorHandlerService.handle)
+      );
+   }
 
-    /**
-     * Cup cup matches auto creation
-     * @param {type: 'auto' | 'manual'; to: number; number_of_matches: number} params
-     * @returns {Observable<CupCupMatch[]>}
-     */
-    createCupCupMatchesAuto(params: { type: 'auto' | 'manual'; to: number; number_of_matches: number }): Observable<CupCupMatch[]> {
-        return this.headersWithToken.post(this.cupCupMatchUrl, params).pipe(
-            map(response => response['cup_cup_matches']),
-            catchError(this.errorHandlerService.handle)
-        );
-    }
+   public createCupCupMatch(cupCupMatch: CupCupMatch): Observable<CupCupMatch> {
+      return this.headersWithToken.post(this.cupCupMatchUrl, cupCupMatch).pipe(
+         map(response => response.cup_cup_match),
+         catchError(this.errorHandlerService.handle)
+      );
+   }
 
-    /**
-     * Create cup cup match
-     * @param {CupCupMatch} cupCupMatch
-     * @returns {Observable<CupCupMatch>}
-     */
-    createCupCupMatch(cupCupMatch: CupCupMatch): Observable<CupCupMatch> {
-        return this.headersWithToken.post(this.cupCupMatchUrl, cupCupMatch).pipe(
-            map(response => response['cup_cup_match']),
-            catchError(this.errorHandlerService.handle)
-        );
-    }
+   public updateCupCupMatch(cupCupMatch: CupCupMatch, cupCupMatchId: number): Observable<CupCupMatch> {
+      return this.headersWithToken.put(`${this.cupCupMatchUrl}/${cupCupMatchId}`, cupCupMatch).pipe(
+         map(response => response.cup_cup_match),
+         catchError(this.errorHandlerService.handle)
+      );
+   }
 
-    /**
-     * Update cup cup match
-     * @param {CupCupMatch} cupCupMatch
-     * @param {number} cupCupMatchId
-     * @returns {Observable<CupCupMatch>}
-     */
-    updateCupCupMatch(cupCupMatch: CupCupMatch, cupCupMatchId: number): Observable<CupCupMatch> {
-        return this.headersWithToken.put(`${this.cupCupMatchUrl}/${cupCupMatchId}`, cupCupMatch).pipe(
-            map(response => response['cup_cup_match']),
-            catchError(this.errorHandlerService.handle)
-        );
-    }
-
-    /**
-     * Delete cup cup match
-     * @param {number} cupCupMatchId
-     * @returns {Observable<void>}
-     */
-    deleteCupCupMatch(cupCupMatchId: number): Observable<void> {
-        return this.headersWithToken.delete(`${this.cupCupMatchUrl}/${cupCupMatchId}`).pipe(catchError(this.errorHandlerService.handle));
-    }
+   public deleteCupCupMatch(cupCupMatchId: number): Observable<void> {
+      return this.headersWithToken.delete(`${this.cupCupMatchUrl}/${cupCupMatchId}`).pipe(catchError(this.errorHandlerService.handle));
+   }
 }
--- src/app/core/services/cup/cup-stage.service.ts
+++ src/app/core/services/cup/cup-stage.service.ts
@@ -1,92 +1,68 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
-import { catchError, map } from 'rxjs/operators';
 import { CupStage } from '@models/cup/cup-stage.model';
-import { environment } from 'environments/environment';
 import { ErrorHandlerService } from '@services/error-handler.service';
 import { HeadersWithToken } from '@services/headers-with-token.service';
-import { isNullOrUndefined } from 'util';
+import { environment } from 'environments/environment';
 import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { isNullOrUndefined } from 'util';
 
 @Injectable()
 export class CupStageService {
-    constructor(
-        private errorHandlerService: ErrorHandlerService,
-        private headersWithToken: HeadersWithToken,
-        private httpClient: HttpClient
-    ) {}
+   private cupStageUrl = `${environment.apiUrl}cup/stages`;
+   constructor(
+      private errorHandlerService: ErrorHandlerService,
+      private headersWithToken: HeadersWithToken,
+      private httpClient: HttpClient
+   ) {}
 
-    private cupStageUrl = `${environment.apiUrl}cup/stages`;
+   /**
+    * @deprecated
+    */
+   public getCupStages(page?: number, active?: boolean, ended?: boolean, competitionId?: number): Observable<any> {
+      let params = new HttpParams();
+      if (page) {
+         params = params.append('page', page.toString());
+      }
+      if (!isNullOrUndefined(active)) {
+         params = params.append('active', active ? '1' : '0');
+      }
+      if (!isNullOrUndefined(ended)) {
+         params = params.append('ended', ended ? '1' : '0');
+      }
+      if (!isNullOrUndefined(competitionId)) {
+         params = params.append('competition_id', competitionId.toString());
+      }
+      return this.httpClient.get(this.cupStageUrl, { params }).pipe(catchError(this.errorHandlerService.handle));
+   }
 
-    /**
-     * Get cup stages
-     * @param {number} page
-     * @param {boolean} active
-     * @param {boolean} ended
-     * @param {number} competitionId
-     * @returns {Observable<any>}
-     */
-    getCupStages(page?: number, active?: boolean, ended?: boolean, competitionId?: number): Observable<any> {
-        let params = new HttpParams();
-        if (page) {
-            params = params.append('page', page.toString());
-        }
-        if (!isNullOrUndefined(active)) {
-            params = params.append('active', active ? '1' : '0');
-        }
-        if (!isNullOrUndefined(ended)) {
-            params = params.append('ended', ended ? '1' : '0');
-        }
-        if (!isNullOrUndefined(competitionId)) {
-            params = params.append('competition_id', competitionId.toString());
-        }
-        return this.httpClient.get(this.cupStageUrl, { params: params }).pipe(catchError(this.errorHandlerService.handle));
-    }
+   /**
+    * @deprecated
+    */
+   public getCupStage(cupStageId: number): Observable<CupStage> {
+      return this.httpClient.get(`${this.cupStageUrl}/${cupStageId}`).pipe(
+         map(response => response.cup_stage),
+         catchError(this.errorHandlerService.handle)
+      );
+   }
 
-    /**
-     * Get cup stage by id
-     * @param {number} cupStageId
-     * @returns {Observable<CupStage>}
-     */
-    getCupStage(cupStageId: number): Observable<CupStage> {
-        return this.httpClient.get(`${this.cupStageUrl}/${cupStageId}`).pipe(
-            map(response => response['cup_stage']),
-            catchError(this.errorHandlerService.handle)
-        );
-    }
+   public createCupStage(cupStage: CupStage): Observable<CupStage> {
+      return this.headersWithToken.post(this.cupStageUrl, cupStage).pipe(
+         map(response => response.cup_stage),
+         catchError(this.errorHandlerService.handle)
+      );
+   }
 
-    /**
-     * Create cup stage
-     * @param {CupStage} cupStage
-     * @returns {Observable<CupStage>}
-     */
-    createCupStage(cupStage: CupStage): Observable<CupStage> {
-        return this.headersWithToken.post(this.cupStageUrl, cupStage).pipe(
-            map(response => response['cup_stage']),
-            catchError(this.errorHandlerService.handle)
-        );
-    }
+   public updateCupStage(cupStage: CupStage, cupStageId: number): Observable<CupStage> {
+      return this.headersWithToken.put(`${this.cupStageUrl}/${cupStageId}`, cupStage).pipe(
+         map(response => response.cup_stage),
+         catchError(this.errorHandlerService.handle)
+      );
+   }
 
-    /**
-     * Update cup stage
-     * @param {CupStage} cupStage
-     * @param {number} cupStageId
-     * @returns {Observable<CupStage>}
-     */
-    updateCupStage(cupStage: CupStage, cupStageId: number): Observable<CupStage> {
-        return this.headersWithToken.put(`${this.cupStageUrl}/${cupStageId}`, cupStage).pipe(
-            map(response => response['cup_stage']),
-            catchError(this.errorHandlerService.handle)
-        );
-    }
-
-    /**
-     * Delete cup stage
-     * @param {number} cupStageId
-     * @returns {Observable<void>}
-     */
-    deleteCupStage(cupStageId: number): Observable<void> {
-        return this.headersWithToken.delete(`${this.cupStageUrl}/${cupStageId}`).pipe(catchError(this.errorHandlerService.handle));
-    }
+   public deleteCupStage(cupStageId: number): Observable<void> {
+      return this.headersWithToken.delete(`${this.cupStageUrl}/${cupStageId}`).pipe(catchError(this.errorHandlerService.handle));
+   }
 }
