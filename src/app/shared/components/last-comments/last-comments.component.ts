ee7c1a7c2099b0f15e02dcd7844df3bdb03dc227
Andriy Shostak <andrew.shostak11@gmail.com>
2021-03-20 17:39:24 +0200

    Use v2/comments endpoint in last-comments component

--- src/app/shared/components/last-comments/last-comments.component.ts
+++ src/app/shared/components/last-comments/last-comments.component.ts
@@ -1,40 +1,43 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-import { Comment } from '@models/comment.model';
-import { CommentService } from '../../../news/shared/comment.service';
-import { RequestParams } from '@models/request-params.model';
+import { CommentNewService } from '@app/news/shared/comment-new.service';
+import { Sequence } from '@enums/sequence.enum';
+import { CommentNew } from '@models/new/comment-new.model';
+import { CommentSearch } from '@models/search/comment-search.model';
 
 @Component({
-    selector: 'app-last-comments',
-    templateUrl: './last-comments.component.html',
-    styleUrls: ['./last-comments.component.scss']
+   selector: 'app-last-comments',
+   templateUrl: './last-comments.component.html',
+   styleUrls: ['./last-comments.component.scss']
 })
 export class LastCommentsComponent implements OnInit {
-    constructor(private commentService: CommentService) {}
+   constructor(private commentService: CommentNewService) {}
 
-    @Input() limitTo = 100;
+   @Input() limitTo = 100;
 
-    comments: Comment[];
+   comments: CommentNew[];
 
-    shortenText(text: string): string {
-        if (this.limitTo === 0) {
-            return text;
-        }
+   shortenText(text: string): string {
+      if (this.limitTo === 0) {
+         return text;
+      }
 
-        const shortenText = text.slice(0, this.limitTo);
-        return shortenText + (text.length > this.limitTo ? '...' : '');
-    }
+      const shortenText = text.slice(0, this.limitTo);
+      return shortenText + (text.length > this.limitTo ? '...' : '');
+   }
 
-    ngOnInit() {
-        this.getCommentsData();
-    }
+   ngOnInit() {
+      this.getCommentsData();
+   }
 
-    private getCommentsData(): void {
-        const requestParams: RequestParams[] = [
-            { parameter: 'limit', value: '3' },
-            { parameter: 'order_by', value: 'created_at' },
-            { parameter: 'sequence', value: 'desc' }
-        ];
-        this.commentService.getComments(requestParams).subscribe(response => (this.comments = response));
-    }
+   private getCommentsData(): void {
+      const search: CommentSearch = {
+         sequence: Sequence.Descending,
+         orderBy: 'created_at',
+         limit: 3,
+         page: 1,
+         relations: ['user']
+      };
+      this.commentService.getComments(search).subscribe(response => (this.comments = response.data));
+   }
 }
