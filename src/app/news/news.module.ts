053ef0b9591422cf9587759e2f57a8a9a33a3bd9
Andriy Shostak <andrew.shostak11@gmail.com>
2021-03-20 18:01:52 +0200

    Remove old comment service

--- src/app/news/news.module.ts
+++ src/app/news/news.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { CommentService } from './shared/comment.service';
 import { NewsComponent } from './news.component';
 import { NewsDetailComponent } from './news-detail/news-detail.component';
 import { NewsListComponent } from './news-list/news-list.component';
@@ -15,7 +14,7 @@ import { CommentFormComponent } from '@app/news/shared/comment-form/comment-form
 @NgModule({
    imports: [CommonModule, ReactiveFormsModule, NewsRoutingModule, SharedModule],
    declarations: [NewsComponent, NewsDetailComponent, NewsListComponent, CommentFormComponent],
-   providers: [NewsService, CommentService, CommentNewService],
+   providers: [NewsService, CommentNewService],
    exports: [NewsComponent]
 })
 export class NewsModule {}
