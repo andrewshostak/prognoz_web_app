adf109fd7e4858a58a94da8ee4386f0127f5aba6
Andriy Shostak <andrew.shostak11@gmail.com>
2021-12-25 13:28:03 +0200

    Update pusher.js version, sort users list

--- src/app/core/services/pusher.service.ts
+++ src/app/core/services/pusher.service.ts
@@ -1,55 +1,52 @@
 import { Injectable } from '@angular/core';
 
 import { environment } from '@env';
-
-declare const Pusher: any;
+import Pusher from 'pusher-js';
 
 @Injectable()
 export class PusherService {
-    /**
-     * Bind event to channel
-     * @param subscription
-     * @param {string} eventName
-     * @param callback
-     */
-    bindEvent(subscription, eventName: string, callback) {
-        return subscription.bind(eventName, callback);
-    }
+   /**
+    * Bind event to channel
+    * @param subscription
+    * @param {string} eventName
+    * @param callback
+    */
+   bindEvent(subscription, eventName: string, callback) {
+      return subscription.bind(eventName, callback);
+   }
 
-    /**
-     * Create Pusher instance
-     * @returns {any}
-     */
-    createInstance() {
-        const pusher = new Pusher(environment.pusher.apiKey, {
-            cluster: 'eu',
-            encrypted: false,
-            authEndpoint: environment.apiUrl + 'auth/pusher',
-            auth: {
-                headers: {
-                    Authorization: 'Bearer {' + localStorage.getItem('auth_token') + '}'
-                }
+   /**
+    * Create Pusher instance
+    * @returns {any}
+    */
+   createInstance() {
+      return new Pusher(environment.pusher.apiKey, {
+         cluster: 'eu',
+         authEndpoint: environment.apiUrl + 'auth/pusher',
+         auth: {
+            params: {},
+            headers: {
+               Authorization: 'Bearer {' + localStorage.getItem('auth_token') + '}'
             }
-        });
+         }
+      });
+   }
 
-        return pusher;
-    }
+   /**
+    * Subscribe to channel
+    * @param instance
+    * @param {string} channelName
+    */
+   subscribeToChannel(instance, channelName: string) {
+      return instance.subscribe(channelName);
+   }
 
-    /**
-     * Subscribe to channel
-     * @param instance
-     * @param {string} channelName
-     */
-    subscribeToChannel(instance, channelName: string) {
-        return instance.subscribe(channelName);
-    }
-
-    /**
-     * Unsubscribe from channel
-     * @param instance
-     * @param {string} channelName
-     */
-    unsubscribeFromChannel(instance, channelName: string) {
-        return instance.unsubscribe(channelName);
-    }
+   /**
+    * Unsubscribe from channel
+    * @param instance
+    * @param {string} channelName
+    */
+   unsubscribeFromChannel(instance, channelName: string) {
+      return instance.unsubscribe(channelName);
+   }
 }
--- src/app/shared/components/online-users-list/online-users-list.component.ts
+++ src/app/shared/components/online-users-list/online-users-list.component.ts
@@ -3,28 +3,29 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { User } from '@models/user.model';
 import { CurrentStateService } from '@services/current-state.service';
 import { Subscription } from 'rxjs';
+import { sortBy } from 'lodash';
 
 @Component({
-    selector: 'app-online-users-list',
-    templateUrl: './online-users-list.component.html',
-    styleUrls: ['./online-users-list.component.scss']
+   selector: 'app-online-users-list',
+   templateUrl: './online-users-list.component.html',
+   styleUrls: ['./online-users-list.component.scss']
 })
 export class OnlineUsersListComponent implements OnDestroy, OnInit {
-    constructor(private currentStateService: CurrentStateService) {}
+   constructor(private currentStateService: CurrentStateService) {}
 
-    users: User[] = [];
-    onlineUserSubscription: Subscription;
+   users: User[] = [];
+   onlineUserSubscription: Subscription;
 
-    ngOnDestroy() {
-        if (!this.onlineUserSubscription.closed) {
-            this.onlineUserSubscription.unsubscribe();
-        }
-    }
+   ngOnDestroy() {
+      if (!this.onlineUserSubscription.closed) {
+         this.onlineUserSubscription.unsubscribe();
+      }
+   }
 
-    ngOnInit() {
-        this.users = this.currentStateService.onlineUsers;
-        this.onlineUserSubscription = this.currentStateService.onlineUserObservable.subscribe(() => {
-            this.users = this.currentStateService.onlineUsers;
-        });
-    }
+   ngOnInit() {
+      this.users = this.currentStateService.onlineUsers;
+      this.onlineUserSubscription = this.currentStateService.onlineUserObservable.subscribe(() => {
+         this.users = sortBy(this.currentStateService.onlineUsers, ['name']);
+      });
+   }
 }
