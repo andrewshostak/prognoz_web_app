2e920c1b5625fb13717f342a41a5ce5419b4a3e0
Andriy Shostak <andrew.shostak11@gmail.com>
2022-01-25 18:33:48 +0200

    Remove unused service method

--- src/app/core/services/cup/cup-stage.service.ts
+++ src/app/core/services/cup/cup-stage.service.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { CupStage } from '@models/cup/cup-stage.model';
@@ -11,21 +10,7 @@ import { catchError, map } from 'rxjs/operators';
 @Injectable()
 export class CupStageService {
    private cupStageUrl = `${environment.apiUrl}cup/stages`;
-   constructor(
-      private errorHandlerService: ErrorHandlerService,
-      private headersWithToken: HeadersWithToken,
-      private httpClient: HttpClient
-   ) {}
-
-   /**
-    * @deprecated
-    */
-   public getCupStage(cupStageId: number): Observable<CupStage> {
-      return this.httpClient.get<{ cup_stage: CupStage }>(`${this.cupStageUrl}/${cupStageId}`).pipe(
-         map(response => response.cup_stage),
-         catchError(this.errorHandlerService.handle)
-      );
-   }
+   constructor(private errorHandlerService: ErrorHandlerService, private headersWithToken: HeadersWithToken) {}
 
    public createCupStage(cupStage: CupStage): Observable<CupStage> {
       return this.headersWithToken.post(this.cupStageUrl, cupStage).pipe(
