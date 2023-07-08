f456dfa74487cf405647c9226d3401dbadf829c8
Andriy Shostak <andrew.shostak11@gmail.com>
2023-07-08 18:06:56 +0300

    Rename repository

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ set -e
 ssh-keyscan -H ${SITE_SSH_HOST} >> ~/.ssh/known_hosts
 
 # Copy files via scp
-scp -r ~/prognoz_v2_angular/dist ${SITE_SSH_USER}@${SITE_SSH_HOST}:/var/www/tmp-ui
+scp -r ~/prognoz_web_app/dist ${SITE_SSH_USER}@${SITE_SSH_HOST}:/var/www/tmp-ui
 
 # Change rights and move to files
 ssh ${SITE_SSH_USER}@${SITE_SSH_HOST} 'sudo chown -R root:www-data /var/www/tmp-ui/ && rm -rf /var/www/ui/dist && mv /var/www/tmp-ui/dist/ /var/www/ui/dist/'
