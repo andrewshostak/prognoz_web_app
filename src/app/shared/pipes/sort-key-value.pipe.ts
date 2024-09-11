4dbb155b3d78f028d5619c776cf841be58198cb3
Andriy Shostak <andrew.shostak11@gmail.com>
2024-09-11 22:43:22 +0300

    Fix issue with wrong cup rating columns sorting: introduce sequence param to sortKeyValue pipe

--- src/app/shared/pipes/sort-key-value.pipe.ts
+++ src/app/shared/pipes/sort-key-value.pipe.ts
@@ -7,12 +7,14 @@ import { parseInt } from 'lodash';
    name: 'sortKeyValue'
 })
 export class SortKeyValuePipe implements PipeTransform {
-   transform<T>(items: KeyValue<string, T>[], parseKeyInt: boolean): KeyValue<string, T>[] {
+   transform<T>(items: KeyValue<string, T>[], parseKeyInt: boolean, sequence: 'asc' | 'desc' = 'desc'): KeyValue<string, T>[] {
+      const direction = sequence === 'desc' ? -1 : 1;
+
       return items.sort((a, b) => {
-         if (parseKeyInt) {
-            return parseInt(a.key, 10) > parseInt(b.key, 10) ? -1 : 1;
-         }
-         return a.key > b.key ? -1 : 1;
+         const keyA = parseKeyInt ? parseInt(a.key, 10) : a.key;
+         const keyB = parseKeyInt ? parseInt(b.key, 10) : b.key;
+
+         return direction * (keyA > keyB ? 1 : keyA < keyB ? -1 : 0);
       });
    }
 }
